16c5ea4e25b70c32a13e7bfa009267eefe8d07e4
Nisanech <57728976+Nisanech@users.noreply.github.com>
2023-08-13 20:24:15 -0500

    Seccion header con efecto para la transicion de las imagenes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log("Desde app js");
+//- Swiper efecto cubo
+let swiper = new Swiper(".swiper", {
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  autoplay: {
+    delay: 6000
+  },
+  loop: true
+}) 
